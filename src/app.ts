bbc3cc400fb344d8c20f03210fa5fd42df943deb
Juan Carreño <jcarrenoar@unal.edu.co>
2025-03-02 14:07:48 -0500

    add prometheus route /metrics

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import "dotenv/config"
 import express from "express";
 import router from "./routes.js";
+import promClient  from "prom-client";
+
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 const URI = process.env.URI || "http://localhost";
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
 
 app.use(express.json());
 app.use("/api", router);
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at ${URI}:${PORT}`);
